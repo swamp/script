75b67e8d5ed202edd602c3712e0b132f998511e4
peter.bjorklund <piot@hotmail.com>
2024-11-29 19:25:25 +0100

    fix: minor fixes

--- crates/swamp-script/src/lib.rs
+++ crates/swamp-script/src/lib.rs
@@ -2,10 +2,5 @@
  * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/swamp/script
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
-use std::rc::Rc;
-use swamp_script_analyzer::{ResolveError, Resolver};
-use swamp_script_ast::ModulePath;
-use swamp_script_dep_loader::ParseModule;
-use swamp_script_semantic::{ResolvedModule, ResolvedModuleRef, ResolvedProgram};
 
 pub mod prelude;
